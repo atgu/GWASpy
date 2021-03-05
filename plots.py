df9ef14fc41a2ea64dcb40824d3564d1afaa3262
LindoNkambule <lindonkambule116@gmail.com>
2021-03-06 00:44:24 +0200

    added manhattan and qq plots

--- plots.py
+++ plots.py
@@ -1,4 +1,5 @@
 import hail as hl
+from hail import Table
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -43,6 +44,102 @@ def fstat_plot(df_female, df_male, f_stat_x: float = 0.4, f_stat_y: float = 0.8,
     axs[1].set_ylabel(ylabel='Frequency', fontsize=15)
     axs[1].tick_params(axis='both', which='major', labelsize=12)
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.close()
+
+    return fig
+
+
+def qqplot(pvals, title: str = None, figsize: tuple = (8, 8)):
+    source = pvals._indices.source
+    if isinstance(source, Table):
+        ht = source.select(p_value=pvals)
+    else:
+        ht = source.select_rows(p_value=pvals).rows()
+
+    ht = ht.key_by().select('p_value').key_by('p_value').persist()
+    lambda_gc = hl.lambda_gc(ht['p_value'])
+    n = ht.count()
+    ht = ht.annotate(
+        observed_p=-hl.log10(ht['p_value']),
+        expected_p=-hl.log10((hl.scan.count() + 1) / n),
+        p_val=ht['p_value']
+    ).persist()
+
+    p_val_pd = ht.to_pandas()
+    p_val_pd['observed_p'].values[p_val_pd['observed_p'] > 10] = 10
+    mini = min(p_val_pd['expected_p'].max(), p_val_pd['observed_p'].max())
+    maxi = max(p_val_pd['expected_p'].max(), p_val_pd['observed_p'].max())
+
+    title = f'{title}' if title else 'QQ Plot'
+
+    fig = plt.figure(figsize=figsize)
+    plt.scatter(p_val_pd['expected_p'], p_val_pd['observed_p'], c='black', s=0.5)
+    plt.plot((0, mini), (0, mini), 'red')
+    plt.xlim([0, maxi + 0.5])
+    plt.ylim([0, maxi + 0.5])
+    plt.title(title)
+    plt.ylabel('Observed -log10(' + r'$p$' + ')')
+    plt.xlabel('Expected -log10(' + r'$p$' + ')')
+    plt.close()
+
+    return fig, round(lambda_gc, 3)
+
+
+def manhattan_plot(pvals, significance_threshold: float = -np.log10(5E-08), title: str = None,
+                   figsize: tuple = (17, 11), annotate_sig: bool = False):
+    source = pvals._indices.source
+
+    if isinstance(source, Table):
+        ht = source.select(p_value=pvals)
+    else:
+        ht = source.select_rows(p_value=pvals).rows()
+
+    data = ht.to_pandas()
+    data = data.drop('alleles', 1)  # remove the 'allele' column
+    data.columns = ['chromosome', 'position', 'p']  # rename columns
+    data['chromosome'].replace({"X": 23, "Y": 24, "MT": 25}, inplace=True)
+    data.dropna(subset=['p'], inplace=True)  # drop NAs as log10(val) won't work
+
+    data['-log10(p_value)'] = -np.log10(data['p'])  # compute log10(pvals)
+    data['-log10(p_value)'].values[data['-log10(p_value)'] > 199] = 199
+    data['chromosome'] = data['chromosome'].astype('category')
+    data['ind'] = range(len(data))
+    data_grouped = data.groupby(('chromosome'))
+
+    print(data['-log10(p_value)'].max())
+
+    title = f'{title}' if title else 'Manhattan Plot'
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot()
+
+    x_labels = []
+    x_labels_pos = []
+
+    colors = ['tab:orange', 'tab:blue']
+
+    for num, (name, group) in enumerate(data_grouped):
+        group.plot(kind='scatter', x='ind', y='-log10(p_value)', marker='o', color=colors[int(name) % len(colors)],
+                   ax=ax, s=10)
+        x_labels.append(name)
+        x_labels_pos.append((group['ind'].iloc[-1] - (group['ind'].iloc[-1] - group['ind'].iloc[0]) / 2))
+
+    ax.set_xticks(x_labels_pos)
+    ax.set_xticklabels(x_labels)
+    # ax.set_xlim([0, len(data)])
+    ax.margins(0.05)
+    # ax.set_ylim([0, data['-log10(p_value)'].max() + 1])
+    ax.set_xlabel('Chromosome')
+    plt.title(title)
+    plt.axhline(y=significance_threshold, color='red', linestyle='--', linewidth=2)
+    plt.xticks(fontsize=9, rotation=90)
+    plt.yticks(fontsize=7)
+    if annotate_sig is True:
+        for index, row in data.iterrows():
+            if row['-log10(p_value)'] >= significance_threshold:
+                ax.annotate('{}:{}'.format(row['chromosome'], row['position']),
+                            xy=(index, row['-log10(p_value)'] + 0.1),
+                            bbox=dict(boxstyle="round", fc="0.8"), fontsize=8)
     plt.close()
 
     return fig
