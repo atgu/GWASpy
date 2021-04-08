5b7b10005d9d6e98ae1ba63bcf3ffce717eac991
LindoNkambule <lindonkambule116@gmail.com>
2021-04-08 21:47:10 +0200

    initial upload of PCA directory

---
+++ gwas_py/pca/__init__.py
@@ -0,0 +1,2 @@
+from gwas_py.pca import pca
+__all__ = ['pca']
---
+++ gwas_py/pca/pca.py
@@ -0,0 +1,13 @@
+__author__ = 'Lindo Nkambule'
+
+
+def preimp_qc():
+    print("PCA Coming")
+
+
+def main():
+    preimp_qc()
+
+
+if __name__ == '__main__':
+    main()
