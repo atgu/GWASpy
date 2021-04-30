3b0600eb061c71261e9313da92a3d93ebbc28761
LindoNkambule <lindonkambule116@gmail.com>
2021-04-30 17:10:16 +0200

    added pc_relate method in relatedness_check() and additional filter args in pca_filter_mt()

--- gwaspy/pca/pca_filter_snps.py
+++ gwaspy/pca/pca_filter_snps.py
@@ -7,7 +7,9 @@ def pca_filter_mt(
         in_mt: hl.MatrixTable,
         maf: float = 0.05,
         hwe: float = 1e-3,
-        call_rate: float = 0.98):
+        call_rate: float = 0.98,
+        ld_cor: float = 0.2,
+        ld_window: int = 250000):
 
     print("\nInitial number of SNPs before filtering: {}".format(in_mt.count_rows()))
     mt = hl.variant_qc(in_mt)
@@ -34,7 +36,7 @@ def pca_filter_mt(
     print("\nNumber of SNPs after MHC and chr8 inversions filtering: {}".format(mt_filt.count_rows()))
 
     # This step is expensive (on local machine)
-    mt_ld_prune = hl.ld_prune(mt_filt.GT, r2=0.2, bp_window_size=250000)
+    mt_ld_prune = hl.ld_prune(mt_filt.GT, r2=ld_cor, bp_window_size=ld_window)
     mt_ld_pruned = mt.filter_rows(hl.is_defined(mt_ld_prune[mt.row_key]))
     print("\nNumber of SNPs after LD pruning: {}".format(mt_ld_pruned.count_rows()))
 
@@ -45,42 +47,50 @@ def relatedness_check(
         in_mt: hl.MatrixTable = None,
         method: str = 'ibd',
         outdir: str = None,
-        ibd: float = 0.98):
+        kin_estimate: float = 0.98):
 
-    global mt
+    global mt, samples_to_remove
 
     in_mt = hl.variant_qc(in_mt)
     in_mt = hl.sample_qc(in_mt)
 
+    # _localize=False means don't put this in Python, keep it as a Hail expr
+    call_rate_dict = in_mt.aggregate_cols(hl.dict(hl.agg.collect((in_mt.s, in_mt.sample_qc.call_rate))), _localize=False)
+
     if method == 'pc_relate':
         print("\nUsing PC-Relate for relatedness checks")
+        relatedness_ht = hl.pc_relate(in_mt.GT, 0.01, k=10, min_kinship=0.1, statistics='kin')
+        samples_to_remove_ht = relatedness_ht.filter(relatedness_ht.kin > kin_estimate)
+
+        # get call rates for both samples so we remove the one with lower call rate between the two
+        samples_to_remove = samples_to_remove_ht.annotate(cr_s1=call_rate_dict[samples_to_remove_ht.i.s],
+                                                          cr_s2=call_rate_dict[samples_to_remove_ht.j.s])
 
     elif method == 'ibd':
         print("\nUsing PLINK-style identity by descent for relatedness checks")
-        mt = in_mt.annotate_rows(maf=hl.min(in_mt.variant_qc.AF))
-        relatedness_ht = hl.identity_by_descent(mt, maf=mt['maf'])  # this returns a Hail Table with the sample pairs
-        samples_to_remove_ht = relatedness_ht.filter(relatedness_ht.ibd.PI_HAT > ibd)
+        in_mt = in_mt.annotate_rows(maf=hl.min(in_mt.variant_qc.AF))
+        relatedness_ht = hl.identity_by_descent(in_mt, maf=in_mt['maf'])  # this returns a Hail Table with the sample pairs
+        samples_to_remove_ht = relatedness_ht.filter(relatedness_ht.ibd.PI_HAT > kin_estimate)
 
-        # _localize=False means don't put this in Python, keep it as a Hail expr
-        call_rate_dict = mt.aggregate_cols(hl.dict(hl.agg.collect((mt.s, mt.sample_qc.call_rate))), _localize=False)
         # get call rates for both samples so we remove the one with lower call rate between the two
         samples_to_remove = samples_to_remove_ht.annotate(cr_s1=call_rate_dict[samples_to_remove_ht.i],
                                                           cr_s2=call_rate_dict[samples_to_remove_ht.j])
-        samples_list = samples_to_remove.annotate(sample_to_remove=hl.cond(
-            samples_to_remove.cr_s1 >= samples_to_remove.cr_s2, samples_to_remove.i, samples_to_remove.j))
-        samples = samples_list.sample_to_remove.collect()
-
-        if len(samples) > 0:
-            mt = mt.filter_cols(hl.literal(samples).contains(mt['s']), keep=False)
-            print("Number of samples removed after relatedness checks: {}".format(len(samples)))
-            with open(outdir + 'relatedness_removed_samples.tsv', 'w') as f:
-                for sample in samples:
-                    f.write(sample + "\n")
-
-        else:
-            print("No samples failed the relatedness check")
 
     else:
         print("\nUsing KING for relatedness checks")
 
-    return mt
+    samples_list = samples_to_remove.annotate(sample_to_remove=hl.cond(
+        samples_to_remove.cr_s1 >= samples_to_remove.cr_s2, samples_to_remove.i, samples_to_remove.j))
+    samples = samples_list.sample_to_remove.collect()
+
+    if len(samples) > 0:
+        in_mt = in_mt.filter_cols(hl.literal(samples).contains(in_mt['s']), keep=False)
+        print("Number of samples removed after relatedness checks: {}".format(len(samples)))
+        with open(outdir + 'relatedness_removed_samples.tsv', 'w') as f:
+            for sample in samples:
+                f.write(sample + "\n")
+
+    else:
+        print("No samples failed the relatedness check")
+
+    return in_mt
