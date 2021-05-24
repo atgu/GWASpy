7711acadf1f3db2688f295bd83a1e717b0102583
LindoNkambule <lindonkambule116@gmail.com>
2021-05-24 15:47:36 +0200

    include fam/pat/mat_id, rsid, pheno, and sex when exporting to PLINK

--- gwaspy/utils/export_file.py
+++ gwaspy/utils/export_file.py
@@ -8,7 +8,9 @@ def export_qced_file(mt: hl.MatrixTable, out_dir: str, basename: str, export_typ
         mt.write('{}GWASpy/Preimp_QC/{}.mt'.format(out_dir, outname), overwrite=True)
 
     elif export_type == 'plink':
-        hl.export_plink(mt, '{}GWASpy/Preimp_QC/{}'.format(out_dir, outname))
+        hl.export_plink(dataset=mt, output='{}GWASpy/Preimp_QC/{}'.format(out_dir, outname), fam_id=mt.fam_id,
+                        ind_id=mt.s, pat_id=mt.pat_id, mat_id=mt.mat_id, is_female=mt.is_female, pheno=mt.is_case,
+                        varid=mt.rsid)
 
     else:
         hl.export_vcf(mt, '{}GWASpy/Preimp_QC/{}.vcf.bgz'.format(out_dir, outname))
