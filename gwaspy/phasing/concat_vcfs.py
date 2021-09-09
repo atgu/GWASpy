d7ce618c36735e0e2bbaff7b7c14c846b66b398a
LindoNkambule <lindonkambule116@gmail.com>
2021-09-09 17:15:27 +0200

    add Batch function for running concatenate

--- gwaspy/phasing/concat_vcfs.py
+++ gwaspy/phasing/concat_vcfs.py
@@ -1,8 +1,12 @@
 __author__ = 'Michael Wilson & Lindo Nkambule'
 
 import hailtop.batch as hb
-from typing import List
+import hail as hl
+import pandas as pd
 from gwaspy.utils.get_file_size import bytes_to_gb
+from gwaspy.phasing.get_filebase import get_vcf_filebase
+from typing import List
+from typing import Union
 
 
 def concat_vcfs(b: hb.batch.Batch,
@@ -45,3 +49,27 @@ def concat_vcfs(b: hb.batch.Batch,
 
     concat.command(f'mv {out_filename} {concat.ofile}')
     b.write_output(concat.ofile, f'{out_dir}/GWASpy/Phasing/{vcf_basename}/phased_merged')
+
+
+def run_concat(backend: Union[hb.ServiceBackend, hb.LocalBackend] = None,
+               input_vcfs: str = None,
+               output_type: str = 'bcf',
+               out_dir: str = None):
+
+    concat_b = hb.Batch(backend=backend, name=f'concat-phased-chunks')
+
+    vcf_paths = pd.read_csv(input_vcfs, sep='\t', header=None)
+
+    for index, row in vcf_paths.iterrows():
+        vcf = row[0]
+        vcf_filebase = get_vcf_filebase(vcf)
+
+        phased_vcfs_chunks = hl.utils.hadoop_ls(f'{out_dir}/GWASpy/Phasing/{vcf_filebase}/phased_scatter')
+
+        vcfs = []
+        for i in phased_vcfs_chunks:
+            vcfs.append(i['path'])
+
+        concat_vcfs(b=concat_b, vcfs_to_merge=vcfs, vcf_basename=vcf_filebase, output_type=output_type, out_dir=out_dir)
+
+    concat_b.run()
