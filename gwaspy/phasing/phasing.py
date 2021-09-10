b1d4c07b660526a8faa8711c4033f9bd10572bea
LindoNkambule <lindonkambule116@gmail.com>
2021-09-10 16:16:43 +0200

    split scatter,phasing,concat into separate steps

--- gwaspy/phasing/phasing.py
+++ gwaspy/phasing/phasing.py
@@ -1,121 +1,7 @@
 __author__ = 'Michael Wilson & Lindo Nkambule'
 
 import hailtop.batch as hb
-import hail as hl
 import argparse
-import pandas as pd
-from gwaspy.phasing.get_filebase import get_vcf_filebase
-from gwaspy.phasing.scatter_vcf import create_windows_bed, vcf_scatter
-from typing import Union
-
-
-def eagle_phasing(b: hb.batch.Batch,
-                  vcf_file: str = None,
-                  ref_vcf: hb.resource.ResourceFile = None,
-                  reference: str = 'GRCh38',
-                  cpu: int = 8,
-                  memory: str = 'standard',
-                  storage: int = 50,
-                  img: str = 'docker.io/lindonkambule/gwaspy:v1',
-                  threads: int = 16,
-                  out_dir: str = None):
-
-    vcf_filename_no_ext = get_vcf_filebase(vcf_file)
-    output_file_name = f'{vcf_filename_no_ext}.phased.eagle'
-    vcf = b.read_input(vcf_file)
-
-    map_file = '/opt/genetic_map_hg38_withX.txt.gz' if reference == 'GRCh38' else '/opt/genetic_map_hg19_withX.txt.gz'
-
-    phase = b.new_job(name=output_file_name)
-    phase.cpu(cpu)
-    phase.memory(memory)
-    phase.storage(f'{storage}Gi')
-    phase.image(img)
-
-    if ref_vcf:
-        cmd = f'''
-        eagle \
-            --geneticMapFile {map_file} \
-            --numThreads {threads} \
-            --outPrefix {output_file_name} \
-            --vcfOutFormat z \
-            --vcfRef {ref_vcf} \
-            --vcfTarget {vcf}
-        '''
-
-    else:
-        cmd = f'''
-        eagle \
-            --geneticMapFile {map_file} \
-            --numThreads {threads} \
-            --outPrefix {output_file_name} \
-            --vcfOutFormat z \
-            --vcf {vcf}
-        '''
-
-    phase.command(cmd)
-
-    phase.command(f'mv {output_file_name}.vcf.gz {phase.ofile}')
-    b.write_output(phase.ofile, f'{out_dir}/{output_file_name}.vcf.gz')
-
-    return phase
-
-
-def shapeit_phasing(b: hb.batch.Batch,
-                    vcf_file: str = None,
-                    ref_vcf: hb.resource.ResourceFile = None,
-                    reference: str = 'GRCh38',
-                    region: Union[str, int] = None,
-                    cpu: int = 8,
-                    memory: str = 'standard',
-                    storage: int = 50,
-                    img: str = 'docker.io/lindonkambule/gwaspy:v1',
-                    threads: int = 16,
-                    out_dir: str = None):
-
-    vcf_filename_no_ext = get_vcf_filebase(vcf_file)
-    output_file_name = f'{vcf_filename_no_ext}.phased.shapeit.vcf.gz'
-    vcf = b.read_input(vcf_file)
-
-    map_file = '/opt/genetic_map_hg38_withX.txt.gz' if reference == 'GRCh38' else '/opt/genetic_map_hg19_withX.txt.gz'
-
-    phase = b.new_job(name=output_file_name)
-    phase.cpu(cpu)
-    phase.memory(memory)
-    phase.storage(f'{storage}Gi')
-    phase.image(img)
-
-    if ref_vcf:
-        # shapeit requires that the VCF be indexed
-        phase.command(f'bcftools index {ref_vcf}')
-        cmd = f'''
-        shapeit4.2 \
-            --input {vcf} \
-            --map {map_file} \
-            --region {region} \
-            --reference {ref_vcf} \
-            --output {output_file_name} \
-            --thread {threads}
-        '''
-
-    else:
-        cmd = f'''
-        shapeit4.2 \
-            --input {vcf} \
-            --map {map_file} \
-            --region {region} \
-            --output {output_file_name} \
-            --thread {threads}
-        '''
-
-    # shapeit requires that the VCF be indexed
-    phase.command(f'bcftools index {vcf}')
-    phase.command(cmd)
-
-    phase.command(f'mv {output_file_name} {phase.ofile}')
-    b.write_output(phase.ofile, f'{out_dir}/{output_file_name}')
-
-    return phase
 
 
 def haplotype_phasing(input_vcfs: str = None,
@@ -128,24 +14,21 @@ def haplotype_phasing(input_vcfs: str = None,
                       max_win_size_cm: float = 10.0,
                       overlap_size_cm: float = 2.0,
                       scatter_memory: int = 26,
-                      cpu: int = 8,
-                      memory: str = 'standard',
-                      storage: int = 50,
-                      threads: int = 16,
+                      cpu: int = 4,
+                      threads: int = 3,
+                      run: str = 'scatter',
+                      output_type: str = 'bcf',
                       out_dir: str = None):
     # Error handling
-    if software.lower() not in ['eagle', 'shapeit']:
-        raise SystemExit(f'Incorrect software {software} selected. Options are [eagle, shapeit]')
-
-    if reference not in ['GRCh37', 'GRCh38']:
-        raise SystemExit(f'Incorrect reference genome build {reference} selected. Options are [GRCh37, GRCh38]')
-
-    if memory not in ['lowmem', 'standard', 'highmem']:
-        raise SystemExit(f'Incorrect memory type {memory} selected. Options are [lowmem, standard, highmem]')
-
     if not out_dir:
         raise SystemExit('Output directory not specified. Specify using --out_dir if running from the command line or'
                          'out_dir argument if running inside a Python script')
+
+    if run.lower() not in ['scatter', 'phase', 'concat']:
+        raise SystemExit(f'Incorrect process {run} selected. Options are [scatter, phase, concat]')
+
+    if output_type.lower() not in ['bcf', 'vcf']:
+        raise SystemExit(f'Incorrect output type {run} selected. Options are [bcf, vcf]')
 
     if local:
         backend = hb.LocalBackend()
@@ -153,66 +36,22 @@ def haplotype_phasing(input_vcfs: str = None,
         backend = hb.ServiceBackend(billing_project=billing_project,
                                     bucket=bucket)
 
-    ###################################### SCATTER VCF FILE ######################################
-    create_windows_bed(reference=reference, max_win_size_cm=max_win_size_cm, out_dir=out_dir,
-                       overlap_size_cm=overlap_size_cm)
+    # Scatter VCF/BCF file(s)
+    if run.lower() == 'scatter':
+        from gwaspy.phasing.scatter_vcf import run_scatter
+        run_scatter(backend=backend, input_vcfs=input_vcfs, reference=reference, max_win_size_cm=max_win_size_cm,
+                    overlap_size_cm=overlap_size_cm, scatter_memory=scatter_memory, out_dir=out_dir)
 
-    scatter_job = hb.Batch(backend=backend, name='scatter-vcf')
+    # Phase scatterd chunks
+    if run.lower() == 'phase':
+        from gwaspy.phasing.phase_vcf import run_phase
+        run_phase(backend=backend, input_vcfs=input_vcfs, vcf_ref_path=vcf_ref, software=software, reference=reference,
+                  cpu=cpu, threads=threads, out_dir=out_dir)
 
-    vcf_paths = pd.read_csv(input_vcfs, sep='\t', header=None)
-
-    for index, row in vcf_paths.iterrows():
-        vcf = row[0]
-
-        vcf_scatter(b=scatter_job, vcf_file=vcf, intervals_bed=f'{out_dir}/GWASpy/Phasing/gwaspy.refscatter.bed',
-                    memory=scatter_memory, out_dir=out_dir)
-
-    scatter_job.run()
-
-    ######################################  PHASING ######################################
-
-    phasing = hb.Batch(backend=backend,
-                       name=f'haplotype-phasing-{software}')
-
-    if vcf_ref:
-        vcf_ref = phasing.read_input(vcf_ref)
-        print('RUNNING PHASING WITH A REFERENCE PANEL\n')
-    else:
-        vcf_ref = None
-        print('RUNNING PHASING WITHOUT A REFERENCE PANEL\n')
-
-    vcf_paths = pd.read_csv(input_vcfs, sep='\t', header=None)
-
-    # get the regions so we can map each file to its specific region
-    regions = pd.read_csv(f'{out_dir}/GWASpy/Phasing/regions.lines', sep='\t', names=['reg', 'ind'])
-    regions_dict = pd.Series(regions.reg.values, index=regions.ind).to_dict()
-
-    for index, row in vcf_paths.iterrows():
-        vcf = row[0]
-        vcf_filebase = get_vcf_filebase(vcf)
-        scatter_vcfs_paths = hl.utils.hadoop_ls(f'{out_dir}/GWASpy/Phasing/{vcf_filebase}/scatter_vcfs')
-
-        vcfs = []
-        for i in scatter_vcfs_paths:
-            vcfs.append(i['path'])
-
-        phased_vcf_out_dir = f'{out_dir}/GWASpy/Phasing/{vcf_filebase}/phased_scatter'
-
-        for file in vcfs:
-            # get specific region for file using regions.line file
-            vcf_basename = get_vcf_filebase(file)
-            file_index = int(vcf_basename.split('.')[-1])
-            file_region = regions_dict[file_index]
-
-            if software == 'eagle':
-                eagle_phasing(b=phasing, vcf_file=file, ref_vcf=vcf_ref, reference=reference, cpu=cpu, memory=memory,
-                              storage=storage, threads=threads, out_dir=phased_vcf_out_dir)
-
-            else:
-                shapeit_phasing(b=phasing, vcf_file=file, ref_vcf=vcf_ref, reference=reference, region=file_region,
-                                cpu=cpu, memory=memory, storage=storage, threads=threads, out_dir=phased_vcf_out_dir)
-
-    phasing.run()
+    # Concatenate phased chunks
+    if run.lower() == 'concat':
+        from gwaspy.phasing.concat_vcfs import run_concat
+        run_concat(backend=backend, input_vcfs=input_vcfs, output_type=output_type, out_dir=out_dir)
 
 
 def main():
@@ -226,11 +65,11 @@ def main():
     parser.add_argument('--reference', type=str, default='GRCh38', choices=['GRCh37', 'GRCh38'])
     parser.add_argument('--max-win-size-cm', type=float, default=10.0)
     parser.add_argument('--overlap-size-cm', type=float, default=2.0)
-    parser.add_argument('--cpu', type=int, default=8)
+    parser.add_argument('--cpu', type=int, default=4)
     parser.add_argument('--scatter-mem', type=int, default=26)
-    parser.add_argument('--memory', type=str, default='standard', choices=['lowmem', 'standard', 'highmem'])
-    parser.add_argument('--storage', type=int, default=50)
-    parser.add_argument('--threads', type=int, default=16)
+    parser.add_argument('--threads', type=int, default=3)
+    parser.add_argument('--run', type=str, default='standard', choices=['scatter', 'phase', 'concat'])
+    parser.add_argument('--out-type', type=str, default='bcf', choices=['bcf', 'vcf'])
     parser.add_argument('--out-dir', required=True)
 
     args = parser.parse_args()
@@ -238,8 +77,8 @@ def main():
     haplotype_phasing(input_vcfs=args.input_vcfs, vcf_ref=args.vcf_ref, local=args.local, bucket=args.bucket,
                       billing_project=args.billing_project, software=args.software, reference=args.reference,
                       max_win_size_cm=args.max_win_size_cm, overlap_size_cm=args.overlap_size_cm,
-                      scatter_memory=args.scatter_mem, cpu=args.cpu, memory=args.memory, storage=args.storage,
-                      threads=args.threads, out_dir=args.out_dir)
+                      scatter_memory=args.scatter_mem, cpu=args.cpu, threads=args.threads, run=args.run,
+                      output_type=args.out_type, out_dir=args.out_dir)
 
 
 if __name__ == '__main__':
