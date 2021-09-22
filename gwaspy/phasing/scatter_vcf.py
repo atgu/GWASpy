3b33c67732fb26531518988ce75c9e578b38bf7c
LindoNkambule <lindonkambule116@gmail.com>
2021-09-22 23:15:13 +0200

    use correct chrom annotation for GRCh37 to create scatter files

--- gwaspy/phasing/scatter_vcf.py
+++ gwaspy/phasing/scatter_vcf.py
@@ -24,11 +24,10 @@ def create_windows_bed(reference: str = 'GRCh38',
         df_map = pd.read_csv('gs://african-seq-data/GWASpy/maps/genetic_map_hg38_withX.txt.gz', delim_whitespace=True,
                              compression='gzip', header=0, names=['CHR', 'POS', 'RATE', 'CM'])
     else:
-        chrom_lens = {'chr1': 249250621, 'chr2': 243199373, 'chr3': 198022430, 'chr4': 191154276, 'chr5': 180915260,
-                      'chr6': 171115067, 'chr7': 159138663, 'chr8': 146364022, 'chr9': 141213431, 'chr10': 135534747,
-                      'chr11': 135006516, 'chr12': 133851895, 'chr13': 115169878, 'chr14': 107349540,
-                      'chr15': 102531392, 'chr16': 90354753, 'chr17': 81195210, 'chr18': 78077248, 'chr19': 59128983,
-                      'chr20': 63025520, 'chr21': 48129895, 'chr22': 51304566, 'chrX': 155270560}
+        chrom_lens = {'1': 249250621, '2': 243199373, '3': 198022430, '4': 191154276, '5': 180915260, '6': 171115067,
+                      '7': 159138663, '8': 146364022, '9': 141213431, '10': 135534747, '11': 135006516, '12': 133851895,
+                      '13': 115169878, '14': 107349540, '15': 102531392, '16': 90354753, '17': 81195210, '18': 78077248,
+                      '19': 59128983, '20': 63025520, '21': 48129895, '22': 51304566, '23': 155270560}
 
         df_map = pd.read_csv('gs://african-seq-data/GWASpy/maps/genetic_map_hg19_withX.txt.gz', delim_whitespace=True,
                              compression='gzip', header=0, names=['CHR', 'POS', 'RATE', 'CM'])
