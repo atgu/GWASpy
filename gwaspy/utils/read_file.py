8b14903d7b1f98c2cf2e5cd7deb9ee6ebced1585
LindoNkambule <lindonkambule116@gmail.com>
2022-01-20 16:53:57 -0500

    fix for reading VCF files

--- gwaspy/utils/read_file.py
+++ gwaspy/utils/read_file.py
@@ -12,6 +12,7 @@ def read_plink(dirname: str, basename: str) -> hl.MatrixTable:
 
 
 def read_vcf(dirname: str, basename: str, annotations: str) -> hl.MatrixTable:
+    hl._set_flags(no_whole_stage_codegen='1')
     vcf_file = '{}{}.vcf.gz'.format(dirname, basename)
     hl.import_vcf(vcf_file, force_bgz=True, block_size=16).write('{}GWASpy.preimpQC.mt'.format(dirname), overwrite=True)
     in_mt = hl.read_matrix_table('{}GWASpy.preimpQC.mt'.format(dirname))
