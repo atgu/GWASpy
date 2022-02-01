3b4e26013c02e730d04bd9fbd8d563370e522e57
LindoNkambule <lindonkambule116@gmail.com>
2022-02-01 11:52:53 -0500

    move assign_population_pcs() to seperate script for reusability

---
+++ gwaspy/pca/assign_pop_labels.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from typing import Tuple
+
+
+def assign_population_pcs(
+        pop_pc_pd: pd.DataFrame,
+        num_pcs: int,
+        known_col: str = 'SuperPop',
+        fit: RandomForestClassifier = None,
+        seed: int = 42,
+        prop_train: float = 0.8,
+        n_estimators: int = 100,
+        min_prob: float = 0.9,
+        output_col: str = 'pop',
+        missing_label: str = 'oth'
+) -> Tuple[pd.DataFrame, RandomForestClassifier]:
+    """
+    This function uses a random forest model to assign population labels based on the results of PCA.
+    Default values for model and assignment parameters are those used in gnomAD.
+    :param Table pop_pc_pd: Pandas dataframe containing population PCs as well as a column with population labels
+    :param str known_col: Column storing the known population labels
+    :param RandomForestClassifier fit: fit from a previously trained random forest model (i.e., the output from a previous RandomForestClassifier() call)
+    :param int num_pcs: number of population PCs on which to train the model
+    :param int seed: Random seed
+    :param float prop_train: Proportion of known data used for training
+    :param int n_estimators: Number of trees to use in the RF model
+    :param float min_prob: Minimum probability of belonging to a given population for the population to be set (otherwise set to `None`)
+    :param str output_col: Output column storing the assigned population
+    :param str missing_label: Label for samples for which the assignment probability is smaller than `min_prob`
+    :return: Dataframe containing sample IDs and imputed population labels, trained random forest model
+    :rtype: DataFrame, RandomForestClassifier
+    """
+
+    # Expand PC column
+    pc_cols = ['PC{}'.format(i + 1) for i in range(num_pcs)]
+    train_data = pop_pc_pd.loc[~pop_pc_pd[known_col].isnull()]
+
+    N = len(train_data)
+
+    # Split training data into subsamples for fitting and evaluating
+    if not fit:
+        random.seed(seed)
+        train_subsample_ridx = random.sample(list(range(0, N)), int(N * prop_train))
+        train_fit = train_data.iloc[train_subsample_ridx]
+        fit_samples = [x for x in train_fit['s']]
+        evaluate_fit = train_data.loc[~train_data['s'].isin(fit_samples)]
+
+        # Train RF
+        training_set_known_labels = train_fit[known_col].values
+        training_set_pcs = train_fit[pc_cols].values
+        evaluation_set_pcs = evaluate_fit[pc_cols].values
+
+        pop_clf = RandomForestClassifier(n_estimators=n_estimators, random_state=seed)
+        pop_clf.fit(training_set_pcs, training_set_known_labels)
+        print('Random forest feature importances are as follows: {}'.format(pop_clf.feature_importances_))
+
+        # Evaluate RF
+        predictions = pop_clf.predict(evaluation_set_pcs)
+        error_rate = 1 - sum(evaluate_fit[known_col] == predictions) / float(len(predictions))
+        print('Estimated error rate for RF model is {}'.format(error_rate))
+    else:
+        pop_clf = fit
+
+    # Classify data
+    print('Classifying data')
+    pop_pc_pd[output_col] = pop_clf.predict(pop_pc_pd[pc_cols].values)
+    probs = pop_clf.predict_proba(pop_pc_pd[pc_cols].values)
+    probs = pd.DataFrame(probs, columns=[f'prob_{p}' for p in pop_clf.classes_])
+
+    pop_pc_pd = pd.concat([pop_pc_pd.reset_index(drop=True), probs.reset_index(drop=True)], axis=1)
+
+    probs['max'] = probs.max(axis=1)
+    pop_pc_pd.loc[probs['max'] < min_prob, output_col] = missing_label
+
+    return pop_pc_pd, pop_clf
