a64c762f000c3098b070484c27bce5bfa7e93664
LindoNkambule <lindonkambule116@gmail.com>
2022-11-23 15:47:15 -0500

    Write out relatedness statistics to a tsv file

--- gwaspy/pca/pca_filter_snps.py
+++ gwaspy/pca/pca_filter_snps.py
@@ -52,6 +52,9 @@ def relatedness_check(
     if method == 'pc_relate':
         print("\nUsing PC-Relate for relatedness checks")
         relatedness_ht = hl.pc_relate(in_mt.GT, 0.01, k=10, min_kinship=kin_estimate, statistics='kin')
+        
+        print('exporting relatedness statistics to a tsv file')
+        relatedness_ht.export(f'{outdir}relatedness_checks_pc_relate.tsv.bgz')
 
         print('getting related samples to be removed using maximal independent set')
         samples_to_remove = hl.maximal_independent_set(relatedness_ht.i, relatedness_ht.j, False)
@@ -62,6 +65,9 @@ def relatedness_check(
         in_mt = hl.variant_qc(in_mt)
         in_mt = in_mt.annotate_rows(maf=hl.min(in_mt.variant_qc.AF))
         relatedness_ht = hl.identity_by_descent(in_mt, maf=in_mt['maf'], min=kin_estimate)
+        
+        print('exporting relatedness statistics to a tsv file')
+        relatedness_ht.export(f'{outdir}relatedness_checks_pc_relate.tsv.bgz')
 
         print('getting related samples to be removed using maximal independent set')
         samples_to_remove = hl.maximal_independent_set(relatedness_ht.i, relatedness_ht.j, False)
@@ -74,6 +80,9 @@ def relatedness_check(
         relatedness_mt = hl.king(in_mt.GT)
         relatedness_ht = relatedness_mt.filter_entries((relatedness_mt.s_1 != relatedness_mt.s) &
                                                        (relatedness_mt.phi >= kin_estimate)).entries()
+        
+        print('exporting relatedness statistics to a tsv file')
+        relatedness_ht.export(f'{outdir}relatedness_checks_pc_relate.tsv.bgz')
 
         print('getting related samples to be removed using maximal independent set')
         samples_to_remove = hl.maximal_independent_set(relatedness_ht.s_1, relatedness_ht.s, False)
