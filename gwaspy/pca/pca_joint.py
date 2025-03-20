66b0112766671a6b7839a50041d94157903e2f7e
Zan Koenig <znfkoenig@gmail.com>
2025-03-20 11:23:34 -0400

    updated ref in joint pca to public access path for HGDP+1kGP

--- gwaspy/pca/pca_joint.py
+++ gwaspy/pca/pca_joint.py
@@ -7,8 +7,8 @@ import plotly.express as px
 
 
 def joint_pca(
-        ref_dirname: str = 'gs://hgdp-1kg/hgdp_tgp/datasets_for_others/lindo/ds_without_outliers/',
-        ref_basename: str = 'unrelated',
+        ref_dirname: str = 'gs://gcp-public-data--gnomad/release/3.1/secondary_analyses/hgdp_1kg_v2/pca_results/',
+        ref_basename: str = 'unrelateds_without_outliers',
         in_mt: hl.MatrixTable = None,
         data_basename: str = None,
         npcs: int = 20,
